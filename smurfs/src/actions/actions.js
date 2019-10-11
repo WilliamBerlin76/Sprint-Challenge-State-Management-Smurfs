8fe9bb38dc9d3b0030c2508fad97c152f29c4b90
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-11 12:03:34 -0500

    mvp complete

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -4,6 +4,9 @@ export const START_FETCHING = 'START_FETCHING';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
 
+export const START_POST = 'START_POST';
+
+
 export const fetchSmurfs = () => dispatch => {
     dispatch({type: START_FETCHING});
 
@@ -16,4 +19,16 @@ export const fetchSmurfs = () => dispatch => {
     .catch(err => {
         dispatch({ type: FETCH_FAILURE, payload: err.response })
     })
+};
+
+export const postSmurfs = (newSmurf) => dispatch => {
+    axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+            console.log("post failed", err)
+        }
+    )
 }
--- smurfs/src/components/smurfList.js
+++ smurfs/src/components/smurfList.js
@@ -1,16 +1,65 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 
 import {fetchSmurfs} from '../actions/actions';
-
+import {postSmurfs} from '../actions/actions';
 const SmurfList = (props) => {
-    console.log(props.smurfs)
+    const [newSmurf, setNewSmurf] = useState({
+                                        name: '',
+                                        age: null,
+                                        height: ''
+                                    })
+                                    
     useEffect(() => {
         props.fetchSmurfs()
     }, [])
 
+    const handleChangesName = e => {
+        setNewSmurf({name: e.target.value,
+                    age: newSmurf.age,
+                    height: newSmurf.height})
+    };
+    const handleChangesAge = e => {
+        setNewSmurf({name: newSmurf.name,
+                    age: e.target.value,
+                    height: newSmurf.height})
+    };
+    const handleChangesHeight = e => {
+        setNewSmurf({name: newSmurf.name,
+                    age: newSmurf.age,
+                    height: e.target.value})
+    }
+    console.log(newSmurf)
+
+    const submitSmurf = e => {
+        props.postSmurfs(newSmurf)
+    }
     return(
         <>
+        <form>
+            <input
+                placeholder='Name'
+                type='text'
+                name="smurfName"
+                value={newSmurf.name}
+                onChange={handleChangesName}
+            />
+            <input
+                placeholder='Age'
+                type='text'
+                name="smurfAge"
+                value={newSmurf.age}
+                onChange={handleChangesAge}
+            />
+            <input
+                placeholder='Height'
+                type='text'
+                name="smurfHeight"
+                value={newSmurf.height}   
+                onChange={handleChangesHeight}         
+            />
+        </form>
+        <button type='submit' onClick={submitSmurf} >Add Smurf!</button>
         <div className='smurfs-container'>
             {props.smurfs.map(item => {
                 return(
@@ -36,5 +85,5 @@ const mapStateToProps = state => {
 
 export default connect(
     mapStateToProps,
-    { fetchSmurfs }
+    { fetchSmurfs, postSmurfs }
 )(SmurfList)
